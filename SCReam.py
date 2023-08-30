9dc2037f557f3d02216024cec77d99ededa28980
tayko9222 <142625255+tayko9222@users.noreply.github.com>
2023-08-30 17:09:42 +0200

    Add files via upload

---
+++ SCReam.py
@@ -0,0 +1,184 @@
+import time
+import os
+import pystyle
+import colorama
+import colored
+import smtplib
+import requests
+import platform
+import sys
+import webbrowser
+from tqdm import tqdm
+from colored import *
+from pystyle import Colors, Colorate
+from colorama import *
+
+os.system('cls')
+print(f"{Fore.RED}[", end=''), print(f'{colored.fg(172)}+', end=''), print(f"{Fore.RED}]", end=''), print(f'{colored.fg(172)} Please Wait ')
+
+if platform.system().startswith("Linux"):
+    try:
+        import requests
+    except ImportError:
+        os.system("python3 -m pip install requests -q -q -q")
+        import requests
+    try:
+        import colored
+    except ImportError:
+        os.system("python3 -m pip install colored -q -q -q")
+        import colored
+    try:
+        import colorama
+        from colorama import Fore, Back, Style
+    except ImportError:
+        os.system("python3 -m pip install colorama -q -q -q")
+        import colorama
+        from colorama import Fore, Back, Style
+    try:
+        from sys import *
+    except:
+        os.system("python3 -m pip install sys -q -q -q")
+        from sys import *
+    try:
+        import time
+    except:
+        os.system("python3 -m pip install time -q -q -q")
+        from time import *
+elif platform.system().startswith("Windows"):
+    try:
+        import requests
+    except ImportError:
+        os.system("python -m pip install requests -q -q -q")
+        import requests
+    try:
+        import colored
+    except ImportError:
+        os.system("python -m pip install colored -q -q -q")
+        import colored
+    try:
+        import colorama
+        from colorama import Fore, Back, Style
+    except ImportError:
+        os.system("python -m pip install colorama -q -q -q")
+        import colorama
+        from colorama import Fore, Back, Style
+    try:
+        from smtplib import *
+    except:
+        os.system("python -m pip install smtplib -q -q -q")
+        from smtplib import *
+
+time.sleep(1)
+amd = (Colorate.Horizontal(Colors.red_to_yellow,"""
+
+                      ,-,-      
+                     / / |      
+   ,-'             _/ / /       
+  (-_          _,-' `Z_/        
+   "#:      ,-'_,-.    \  _     
+    #'    _(_-'_()\     \" |    
+  ,--_,--'                 |    
+ / ""                      L-'\ 
+ \,--^---v--v-._        /   \ | 
+   \_________________,-'      | 
+                    \          \ 
+                     \          /
+       Loading...     \          /
+
+
+"""))
+
+for char in amd:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.00000234)
+
+time.sleep(2)
+
+for i in tqdm(range(int(9e6))):
+    pass
+
+os.system('cls')
+
+print(Fore.CYAN + "  ____   ___  ____  ____   __   _  _      ____  _  _  ____ ")
+print(Colors.red, "/ ___) / __)(  _ \(  __) / _\ ( \/ )    (_  _)( \/ )(  _ /")
+print(Colors.orange, "\___ \( (__  )   / ) _) /    \/ \/ \      )(   )  (  )   /")
+print(Fore.BLUE + " (____/ \___)(__\_)(____)\_/\_/\_)(_/     (__) (_/\_)(__\_)")
+print(f"{Fore.YELLOW}                                                  [{Fore.BLUE}v.10000{Fore.YELLOW}] ")
+print(f"{Fore.LIGHTBLUE_EX}                                                [{Fore.RED}By {Fore.CYAN}Newark{Fore.LIGHTBLUE_EX}] ")
+print("")
+print("")
+print("")
+print("")
+
+print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}01', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}BruteForce ')
+print("")
+print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}02', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Spamming ')
+print("")
+print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}03', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}DDOS ')
+print("")
+print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}04', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}DoXxing ')
+print("")
+print("")
+print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}0', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Exit ', end=''), print("                ", end=''), print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}9', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Github ', end=''),print("                ", end=''), print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}h', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Help ')
+print("      ")
+print("      ")
+print("      ")
+
+print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}?', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Please choose an option ')
+
+option = int(input("> "))
+
+
+
+
+
+if option == 1:
+     print(f"{Fore.RED}[", end=''), print(f'{colored.fg(172)}+', end=''), print(f"{Fore.RED}]", end=''), print(f'{colored.fg(172)} Please Wait ')
+    
+     time.sleep(3)
+     os.system('cls')
+
+if option == 2:
+     import spm
+
+
+
+if option == 3:
+     print(f"{Fore.RED}[", end=''), print(f'{colored.fg(172)}+', end=''), print(f"{Fore.RED}]", end=''), print(f'{colored.fg(172)} Please Wait ')
+    
+     time.sleep(3)
+     os.system('cls')
+
+if option == 9:
+        print(f"{Fore.CYAN}[", end=''), print(f'{colored.fg(243)}+', end=''), print(f"{Fore.CYAN}]", end=''), print(f'{colored.fg(243)} Redirection in progres... ')
+        time.sleep(1)
+        webbrowser.open('https://github.com/tayko9222/screamtool/')
+
+
+if option == 4:
+     import snd
+
+
+if option == 0:
+     print(f"{Fore.RED}[", end=''), print(f'{colored.fg(172)}+', end=''), print(f"{Fore.RED}]", end=''), print(f'{colored.fg(172)} Leaving In Progres... ')
+    
+     time.sleep(3)
+     os.system('cls')
+     exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                       
