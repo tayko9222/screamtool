b7f5836b6193e16645dfcfbb2459a221b69a853b
newark <142625255+tayko9222@users.noreply.github.com>
2023-09-02 13:46:01 +0200

    Update spmd.py

--- spmd.py
+++ spmd.py
@@ -14,10 +14,20 @@ from colorama import *
 
 
 
-os.system('cls')
+if platform.system().startswith("Linux"):
+    os.system('clear')
+elif platform.system().startswith("Windows"):
+    os.system('cls')
+
 print(f"{Fore.CYAN}[{Fore.RED}+{Fore.CYAN}]{Fore.RED} Please wait ")
+
 time.sleep(3)
-os.system('cls')
+
+if platform.system().startswith("Linux"):
+    os.system('clear')
+elif platform.system().startswith("Windows"):
+    os.system('cls')
+
 
 print(Fore.CYAN + "   ___  _  _   __   __  __        ____  ____   __   _  _  _  _  ____  ____ ")
 print(Colors.red, " / __)( \/ ) / _\ (  )(  )      / ___)(  _ \ / _\ ( \/ )( \/ )(  __)(  _ /")
@@ -33,7 +43,10 @@ input(" > ")
 
 
 
-os.system('cls')
+if platform.system().startswith("Linux"):
+    os.system('clear')
+elif platform.system().startswith("Windows"):
+    os.system('cls')
 
 
 
@@ -50,7 +63,10 @@ if victime==verif:
 else:
     print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}!', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(8)}The email doesnt match please try again ')
     time.sleep(1.5)
-    os.system('cls')
+    if platform.system().startswith("Linux"):
+        os.system('clear')
+    elif platform.system().startswith("Windows"):
+        os.system('cls')
     exit()
 
 
@@ -59,18 +75,28 @@ with smtplib.SMTP('smtp.gmail.com', '587') as smtpserver:
     smtpserver.ehlo()
     smtpserver.starttls()
     smtpserver.ehlo()
-    os.system('cls')
+    if platform.system().startswith("Linux"):
+        os.system('clear')
+    elif platform.system().startswith("Windows"):
+        os.system('cls')
     print((f"{Fore.CYAN}[{Fore.RED}!{Fore.CYAN}]{Fore.RED} Email used is ( noreply6481738240@gmail.com )"))
     smtpserver.login("noreply6481738240@gmail.com", "qtzyckgcfzuotvii")
     time.sleep(1.25)
 
-    os.system('cls')
+    if platform.system().startswith("Linux"):
+        os.system('clear')
+    elif platform.system().startswith("Windows"):
+        os.system('cls')
     print(f"{Fore.CYAN}[{Fore.RED}+{Fore.CYAN}]{Fore.RED} How many mail you want to send ? ", end=''), print(f'{colored.fg(231)} ( Max is 100 )', end=''), print(f'{Fore.RED}')
     for i in range(int(input(" > "))):
         smtpserver.sendmail(victime, verif, message)
         print(f"{Fore.CYAN}[{Fore.RED}+{Fore.CYAN}]{Fore.RED} Number of email who was sent : ", f"{Fore.RED}",i)
         
-os.system('cls')
+    if platform.system().startswith("Linux"):
+        os.system('clear')
+    elif platform.system().startswith("Windows"):
+        os.system('cls')
 print(f"{Fore.BLUE}[{Fore.RED}!{Fore.BLUE}]{Fore.RED} Spam sucess ! ")
+
 
         
