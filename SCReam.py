d1d018e80215922f69bc6f505f8b6e41d154e9ed
newark <142625255+tayko9222@users.noreply.github.com>
2023-09-02 13:45:46 +0200

    Update SCReam.py

--- SCReam.py
+++ SCReam.py
@@ -88,6 +88,11 @@ amd = (Colorate.Horizontal(Colors.red_to_yellow,"""
 
 """))
 
+print("")
+print("")
+print("")
+print("")
+
 for char in amd:
     sys.stdout.write(char)
     sys.stdout.flush()
@@ -98,13 +103,23 @@ time.sleep(2)
 for i in tqdm(range(int(9e6))):
     pass
 
-os.system('cls')
+if platform.system().startswith("Linux"):
+    os.system('clear')
+elif platform.system().startswith("Windows"):
+    os.system('cls')
+
+print(f"{Fore.RED}[", end=''), print(f'{colored.fg(172)}+', end=''), print(f"{Fore.RED}]", end=''), print(f'{colored.fg(172)} Please Wait ')
+
+if platform.system().startswith("Linux"):
+    os.system('clear')
+elif platform.system().startswith("Windows"):
+    os.system('cls')
 
 print(Fore.CYAN + "  ____   ___  ____  ____   __   _  _      ____  _  _  ____ ")
 print(Colors.red, "/ ___) / __)(  _ \(  __) / _\ ( \/ )    (_  _)( \/ )(  _ /")
 print(Colors.orange, "\___ \( (__  )   / ) _) /    \/ \/ \      )(   )  (  )   /")
 print(Fore.BLUE + " (____/ \___)(__\_)(____)\_/\_/\_)(_/     (__) (_/\_)(__\_)")
-print(f"{Fore.YELLOW}                                                  [{Fore.BLUE}v.10000{Fore.YELLOW}] ")
+print(f"{Fore.YELLOW}                                                  [{Fore.BLUE}v.12345{Fore.YELLOW}] ")
 print(f"{Fore.LIGHTBLUE_EX}                                                [{Fore.RED}By {Fore.CYAN}Newark{Fore.LIGHTBLUE_EX}] ")
 print("")
 print("")
@@ -115,7 +130,7 @@ print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}01', end=''), print(f"{F
 print("")
 print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}02', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Spamming ')
 print("")
-print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}03', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}DDOS ')
+print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}03', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Port Scanner ')
 print("")
 print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}04', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}DoXxing ')
 print("")
@@ -137,10 +152,55 @@ if option == 1:
      print(f"{Fore.RED}[", end=''), print(f'{colored.fg(172)}+', end=''), print(f"{Fore.RED}]", end=''), print(f'{colored.fg(172)} Please Wait ')
     
      time.sleep(3)
-     os.system('cls')
+     if platform.system().startswith("Linux"):
+        os.system('clear')
+     elif platform.system().startswith("Windows"):
+        os.system('cls')
 
 if option == 2:
-     import spm
+     if platform.system().startswith("Linux"):
+        os.system('clear')
+     elif platform.system().startswith("Windows"):
+        os.system('cls')
+
+print(Fore.CYAN + "  ____   ___  ____  ____   __   _  _      ____  _  _  ____ ")
+print(Colors.red, "/ ___) / __)(  _ \(  __) / _\ ( \/ )    (_  _)( \/ )(  _ /")
+print(Colors.orange, "\___ \( (__  )   / ) _) /    \/ \/ \      )(   )  (  )   /")
+print(Fore.BLUE + " (____/ \___)(__\_)(____)\_/\_/\_)(_/     (__) (_/\_)(__\_)")
+print(f"{Fore.YELLOW}                                                  [{Fore.BLUE}v.10000{Fore.YELLOW}] ")
+print(f"{Fore.LIGHTBLUE_EX}                                                [{Fore.RED}By {Fore.CYAN}Newark{Fore.LIGHTBLUE_EX}] ")
+print("")
+print("")
+print("")
+print("")
+
+print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}01', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Default Gmail Spam ')
+print("")
+print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}02', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Custom Gmail Spam ')
+print("")
+print("")
+print("")
+print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}0', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Exit ', end=''), print("                ", end=''), print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}9', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Github ', end=''),print("                ", end=''), print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}h', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Help ')
+print("      ")
+print("      ")
+print("      ")
+
+print(f"{Fore.CYAN}[",end=''), print(f'{colored.fg(231)}?', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{colored.fg(84)}Please choose an option ')
+
+opt = int(input("> "))
+
+if opt == 1:
+    if platform.system().startswith("Linux"):
+        os.system('python smpd.py')
+    elif platform.system().startswith("Windows"):
+        import spmd
+
+if opt == 2:
+    if platform.system().startswith("Linux"):
+        os.system('python smpy.py')
+    elif platform.system().startswith("Windows"):
+        import spmy
+    
 
 
 
@@ -148,7 +208,10 @@ if option == 3:
      print(f"{Fore.RED}[", end=''), print(f'{colored.fg(172)}+', end=''), print(f"{Fore.RED}]", end=''), print(f'{colored.fg(172)} Please Wait ')
     
      time.sleep(3)
-     os.system('cls')
+     if platform.system().startswith("Linux"):
+        os.system('clear')
+     elif platform.system().startswith("Windows"):
+        os.system('cls')
 
 if option == 9:
         print(f"{Fore.CYAN}[", end=''), print(f'{colored.fg(243)}+', end=''), print(f"{Fore.CYAN}]", end=''), print(f'{colored.fg(243)} Redirection in progres... ')
@@ -157,14 +220,17 @@ if option == 9:
 
 
 if option == 4:
-     import snd
+     print("Error")
 
 
 if option == 0:
      print(f"{Fore.RED}[", end=''), print(f'{colored.fg(172)}+', end=''), print(f"{Fore.RED}]", end=''), print(f'{colored.fg(172)} Leaving In Progres... ')
     
      time.sleep(3)
-     os.system('cls')
+     if platform.system().startswith("Linux"):
+        os.system('clear')
+     elif platform.system().startswith("Windows"):
+        os.system('cls')
      exit()
 
 
